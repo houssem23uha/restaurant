4299c78a03c268808fb5376f20e503b0ed77f9ce
houssem23uha <72876892+houssem23uha@users.noreply.github.com>
2025-06-15 12:53:16 +0200

    add annimated box with motion framer

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,31 +1,39 @@
-import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { Route, BrowserRouter, Routes, Outlet } from "react-router-dom";
 
 import styles from "./Content.module.scss";
 import Menu from "./Menu";
 import HomeContent from "./HomeContent";
 import FavoriteDishes from "./FavoriteDishes";
+import Reservation from "./Reservation";
+
+const Layout = () => (
+  <div className="d-flex flex flex-column">
+    <div className={`${styles.content} container flex-fill mt-2 p-4`}>
+      <Outlet />
+    </div>
+  </div>
+);
 
 function Content() {
   return (
     <BrowserRouter>
-      <div className={`${styles.appContainer} d-flex flex flex-column`}>
-        <div className={`${styles.content} container flex-fill mt-2 p-4`}>
-          <Routes>
-            <Route path="/" element={<HomeContent />} />
-            <Route path="/menu" element={<Menu />} />
-            <Route path="/menu/items" element={<HomeContent />} />
-            <Route path="/orders" element={<HomeContent />} />
-            <Route path="/orders/history" element={<HomeContent />} />
-            <Route path="/reservations" element={<HomeContent />} />
-            <Route path="/delivery-info" element={<HomeContent />} />
-            <Route path="/user/account" element={<HomeContent />} />
-            <Route path="/user/account/favoris" element={<FavoriteDishes />} />
-            <Route path="/user/account/orders" element={<HomeContent />} />
-            <Route path="/user/account/settings" element={<HomeContent />} />
-            <Route path="/contact" element={<HomeContent />} />
-          </Routes>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/reservations" element={<Reservation />} />
+
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomeContent />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/menu/items" element={<HomeContent />} />
+          <Route path="/orders" element={<HomeContent />} />
+          <Route path="/orders/history" element={<HomeContent />} />
+          <Route path="/delivery-info" element={<HomeContent />} />
+          <Route path="/user/account" element={<HomeContent />} />
+          <Route path="/user/account/favoris" element={<FavoriteDishes />} />
+          <Route path="/user/account/orders" element={<HomeContent />} />
+          <Route path="/user/account/settings" element={<HomeContent />} />
+          <Route path="/contact" element={<HomeContent />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
