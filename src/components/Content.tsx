4aa7eceb62047a407f48932f9bf93ad920ce4902
houssem23uha <72876892+houssem23uha@users.noreply.github.com>
2025-06-15 17:17:41 +0200

    corriger le lien de la nav bar

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,7 +1,7 @@
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 
 import styles from "./Content.module.scss";
-import Menu from "./menu";
+import Menu from "./Menu";
 import HomeContent from "./HomeContent";
 import FavoriteDishes from "./FavoriteDishes";
 
@@ -16,12 +16,12 @@ function Content() {
             <Route path="/menu/items" element={<HomeContent />} />
             <Route path="/orders" element={<HomeContent />} />
             <Route path="/orders/history" element={<HomeContent />} />
-            <Route path="/reservations" element={<HomeContent />} />
+            <Route path="/reservation" element={<HomeContent />} />
             <Route path="/delivery-info" element={<HomeContent />} />
-            <Route path="/user/account" element={<HomeContent />} />
-            <Route path="/user/account/favoris" element={<FavoriteDishes />} />
-            <Route path="/user/account/orders" element={<HomeContent />} />
-            <Route path="/user/account/settings" element={<HomeContent />} />
+            <Route path="/account" element={<HomeContent />} />
+            <Route path="/account/favoris" element={<FavoriteDishes />} />
+            <Route path="/account/orders" element={<HomeContent />} />
+            <Route path="/account/settings" element={<HomeContent />} />
             <Route path="/contact" element={<HomeContent />} />
           </Routes>
         </div>
